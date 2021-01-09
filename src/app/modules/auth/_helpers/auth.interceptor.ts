1ee0bb3ee9dbd1a31b2d8b7a5a836d8ebde410ea
Billy Yanez <bemonio@gmail.com>
2021-01-09 13:10:42 -0500

    feat: logout on error 401

--- src/app/modules/auth/_helpers/auth.interceptor.ts
+++ src/app/modules/auth/_helpers/auth.interceptor.ts
@@ -1,13 +1,17 @@
 import { HTTP_INTERCEPTORS, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 
 import { TokenStorageService } from '../_services/auth-http/token-storage.service';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService,
+    private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
@@ -23,7 +27,23 @@ export class AuthInterceptor implements HttpInterceptor {
         authReq = req.clone({setHeaders: {'Content-Type': 'application/json'}});
       }
     }
-    return next.handle(authReq);
+
+    //send the newly created request
+    return next.handle(authReq)
+    .pipe(
+      catchError(err => {
+        // onError
+        console.log(err);
+        if (err instanceof HttpErrorResponse) {
+            console.log(err.status);
+            console.log(err.statusText);
+            if (err.status === 401) {
+              this.authService.logout();
+            }
+        }
+        return Observable.throw(err);
+      }) as any
+    )
   }
 }
 
