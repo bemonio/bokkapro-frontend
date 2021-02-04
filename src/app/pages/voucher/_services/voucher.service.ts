4f6abf324f22d8316a83d3c5d0be24635b07fe3e
Billy Yanez <bemonio@gmail.com>
2021-02-03 22:46:57 -0500

    refactor: voucher service filter by id

--- src/app/pages/voucher/_services/voucher.service.ts
+++ src/app/pages/voucher/_services/voucher.service.ts
@@ -35,7 +35,7 @@ export class VoucherService {
         }
 
         if (query !== null && query !== undefined && query !== '') {
-            params.append(`filter[]`, String(query));
+            params.append(`filter{id}[]`, String(query));
         }
 
         if (filters !== null && filters !== undefined && filters.length > 0) {
