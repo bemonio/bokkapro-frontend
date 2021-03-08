bfc0ebb120ac60d2f919cbc6b8230fb64a97bafe
Billy Yanez <bemonio@gmail.com>
2021-03-07 23:50:04 -0500

    refactor: validation guide export required fields

--- src/app/pages/guide/guides/guides.component.ts
+++ src/app/pages/guide/guides/guides.component.ts
@@ -83,8 +83,8 @@ export class GuidesComponent implements OnInit {
         this.am_pm_filter = this.searchGroup.controls['am_pm_filter'];
 
         this.exportGroup = fb.group({
-            'date_export': [''],
-            'am_pm_export': [''],
+            'date_export': ['', Validators.compose([Validators.required])],
+            'am_pm_export': ['', Validators.compose([Validators.required])],
         });
         this.date_export = this.exportGroup.controls['date_export'];
         this.am_pm_export = this.exportGroup.controls['am_pm_export'];
