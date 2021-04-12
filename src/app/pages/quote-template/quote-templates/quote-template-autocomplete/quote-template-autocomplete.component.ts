cde1163260bf15b2014a8e6eed87b74962072619
Samuel Buendia <sbuendiapuyo@gmail.com>
2021-04-12 14:20:14 -0500

    correction quote template autocomplete

--- src/app/pages/quote-template/quote-templates/quote-template-autocomplete/quote-template-autocomplete.component.ts
+++ src/app/pages/quote-template/quote-templates/quote-template-autocomplete/quote-template-autocomplete.component.ts
@@ -116,7 +116,7 @@ export class QuoteTemplateAutocompleteComponent implements ControlValueAccessor,
     getModels() {
         this.modelsService.get(this.page, this.per_page, this.sort, this.query, this.filters).toPromise().then(
             response => {
-                this.models = response.currenciesdetails;
+                this.models = response.quote_templates;
                 this.totalRecords = response.meta.total_results;
                 // if (this.model) {
                 //     if (this.model.id) {
