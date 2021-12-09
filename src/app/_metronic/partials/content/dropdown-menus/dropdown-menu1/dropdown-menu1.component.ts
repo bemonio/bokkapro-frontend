9730724c6ee8469fbecb40ac7e4f07e1c1025f5f
Billy Yanez <bemonio@gmail.com>
2021-12-09 11:51:42 -0500

    refactor: get division from crew when driver user

--- src/app/_metronic/partials/content/dropdown-menus/dropdown-menu1/dropdown-menu1.component.ts
+++ src/app/_metronic/partials/content/dropdown-menus/dropdown-menu1/dropdown-menu1.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/modules/auth';
 import { DivisionService } from 'src/app/pages/division/_services';
+import { CrewService } from './../../../../../pages/crew/_services/crew.service';
+import { ToastService } from 'src/app/modules/toast/_services/toast.service';
 
 @Component({
   selector: 'app-dropdown-menu1',
@@ -15,19 +17,55 @@ export class DropdownMenu1Component implements OnInit {
 
   constructor(
     public authService: AuthService,
-    public divisionService: DivisionService
+    public divisionService: DivisionService,
+    public crewService: CrewService,
+    private toastService: ToastService
   ) {
     this.divisions = [];
   }
 
   ngOnInit(): void {
-    this.divisions = this.authService.currentUserValue.employee.divisions;
-    this.division = this.authService.currentDivisionValue;
+    console.log(this.authService.currentUserValue);
+    console.log(this.authService.currentUserValue.groups[0].id);
+    if (this.authService.currentUserValue.groups[0].id == 6) {
+      this.getCrew();
+    } else {
+      this.divisions = this.authService.currentUserValue.employee.divisions;
+      this.division = this.authService.currentDivisionValue;
+    }
   }
 
   changeDivision(division) {
     this.authService.currentDivisionValue = division;
     this.divisionService._change$.next(true);
     this.divisionService._change$.next(false);
+  }
+
+  getCrew() {
+    let page = 1;
+    let per_page = 1;
+    let sort = undefined;
+    let query = undefined;
+    let _with = undefined;
+    let filters = [];
+    filters.push({ key: 'filter{driver}', value: this.authService.currentUserValue.employee.id });
+    filters.push({ key: 'filter{date.icontains}[]', value: '2021-10-10'  });
+
+    this.crewService.get(page, per_page, sort, query, filters, _with).subscribe(
+        response => {
+            this.divisions = response.divisions;
+        },
+        error => {
+            let messageError = [];
+            if (!Array.isArray(error.error)) {
+                messageError.push(error.error);
+            } else {
+                messageError = error.error;
+            }
+            Object.entries(messageError).forEach(
+                ([key, value]) => this.toastService.growl('top-right', 'error', key + ': ' + value)
+            );
+        }
+    );
   }
 }
